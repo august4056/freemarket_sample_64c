e876b079de42f82c25d10bd7aae619212600fa8a
august4056 <57321070+august4056@users.noreply.github.com>
2019-12-03 21:06:48 +0900

    ユニコーンをインストール＆Uglifierをコメントアウト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,10 +22,18 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+
+# Chat-Spaceではコメントアウトした部分です！！
+# メルカリでは必要になるかもしれないところ〜〜
+
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+
+
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
