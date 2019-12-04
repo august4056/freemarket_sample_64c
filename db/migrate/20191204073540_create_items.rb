2faa980edd6acd1cdaa50fa25548ed191a7ba681
tomii1031 <57342235+tomii1031@users.noreply.github.com>
2019-12-04 16:38:32 +0900

    first commit devise

---
+++ db/migrate/20191204073540_create_items.rb
@@ -0,0 +1,19 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name, null:false
+      t.string :image
+      t.text :detail
+      t.text :category
+      t.text :state
+      t.integer :delivery_fee
+      t.text :delivery_area
+      t.integer :delivery_date
+      t.integer :price
+      t.integer :fee
+      t.integer :gross_profit
+
+      t.timestamps
+    end
+  end
+end
