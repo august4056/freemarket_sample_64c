c2cc1272833051e9f304f96fe8853d5c741be0f9
august4056 <57321070+august4056@users.noreply.github.com>
2019-12-05 16:54:31 +0900

    edit indent

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,20 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?     # <=開発環境またはテスト環境なら（env.develop or env.test）ファイルに保存
-  config.storage = :file
+  if Rails.env.development? || Rails.env.test?     # <=開発環境またはテスト環境(env.develop or env.test）ならファイルに保存
+    config.storage = :file
+  
   elsif Rails.env.production?  # <=  本番環境はAWSに保存
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'mercariitems'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercariitems'
+    config.fog_directory  = 'mercariitems'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercariitems'
   end
 end
