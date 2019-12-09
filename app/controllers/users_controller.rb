e10d806973c599dc2d63d3bdd310bc17f07ef99f
NoritakaTakehara <50361226+NoritakaTakehara@users.noreply.github.com>
2019-12-09 13:56:51 +0900

    新規会員登録ページのフロント実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def new
+  end
+  
+  def confirm
+  end
+
+  def address
+  end
+
+  def payment
+  end
+
+  def complete
+  end
+
+  def login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
+  get 'confirm', to: 'users#confirm'
+  get 'address', to: 'users#address'
+  get 'payment', to: 'users#payment'
+  get 'complete', to: 'users#complete'
 end
