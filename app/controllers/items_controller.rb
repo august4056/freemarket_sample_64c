b61c1ecf180efeef6862bb832d84e739c0f7ff48
tomii1031 <57342235+tomii1031@users.noreply.github.com>
2019-12-19 19:10:06 +0900

    とりあえず、コミットします。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,7 @@ class ItemsController < ApplicationController
 
   def create
     @item=Item.new(item_params)
-    @image=Image.new(image_params)
-    if  @item.save
+    if @item.save
         redirect_to root_path
     end 
   end
