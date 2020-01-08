97b82977584d14382af47b93a15c900eb1a5d1e0
august4056 <57321070+august4056@users.noreply.github.com>
2020-01-08 15:27:04 +0900

    マイグレーションエラー直し
    この後はrails db:reset なりdropなりしてください

--- db/migrate/20191204073515_add_name_to_users.rb
+++ db/migrate/20191204073515_add_name_to_users.rb
@@ -1,6 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :name, :string
     add_column :users, :telephone, :integer
     add_column :users, :nickname, :string
   end
